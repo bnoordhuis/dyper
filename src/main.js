ed45500c41d5eff8a0434398ac584146ae96e5e6
Ben Noordhuis <info@bnoordhuis.nl>
2021-01-27 17:23:51 +0100

    apply header->string optimization
    Sad fact: doesn't appear to affect throughput or latency much.

--- src/main.js
+++ src/main.js
@@ -1,8 +1,53 @@
 (function(api, exports) {
   "use strict";
 
+  const KNOWN_METHODS = [
+    "GET",
+    "POST",
+  ];
+
+  const KNOWN_HEADER_NAMES = [
+    "accept-encoding",
+    "content-length",
+    "date",
+    "host",
+  ];
+
+  const KNOWN_HEADER_VALUES = [
+    "0",
+    "1",
+    "2",
+  ];
+
   function handleRequest(method, uri, headers) {
-    return globalThis.onrequest(method, uri, headers);
+    if (typeof method === 'number') {
+      method = KNOWN_METHODS[method];
+    }
+
+    if (typeof uri === 'number') {
+      uri = "/";
+    }
+
+    for (let i = 0, n = headers.length; i < n; i += 2) {
+      let name = headers[i + 0];
+
+      if (typeof name === 'number') {
+        headers[i + 0] = KNOWN_HEADER_NAMES[name];
+      }
+
+      let value = headers[i + 1];
+
+      if (typeof value === 'number') {
+        headers[i + 1] = KNOWN_HEADER_VALUES[value];
+      }
+    }
+
+    headers  = [
+      1 /* "content-length" */, 2 /* "2" */,
+      2 /* "date" */, "Wed, 27 Jan 2021 10:55:19 GMT",
+    ];
+
+    return [200, headers, "ok"];
   }
 
   return handleRequest;
